db9ebca4773f61b714a0f2b33beaaddba0cd7133
Sebin Thomas <sebin.thomas@beo.in>
2026-05-21 00:37:30 +0530

    Update manifest: HTML description and ensure icon

--- bank_reconcile_ext/__manifest__.py
+++ bank_reconcile_ext/__manifest__.py
@@ -6,11 +6,7 @@
     "version": "14.0.1.0.0",
     "category": "Accounting",
     "summary": "Reconcile bank statements with GL account entries",
-    "description": """
-Bank Reconciliation Module
-==========================
-This module allows you to easily reconcile bank statements with GL account entries.
-    """,
+    "description": "<p>Bank Reconciliation Module allows you to easily reconcile bank statements with GL account entries.</p>",
     "author": "Sebin Thomas",
     "depends": [
         'account',
