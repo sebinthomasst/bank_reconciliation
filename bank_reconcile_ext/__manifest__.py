eeb408809779eac0fb85b0cf3e2da1a5e6fe5107
Sebin Thomas <sebin.thomas@beo.in>
2026-05-21 09:30:38 +0530

    Update version for 18.0 branch

--- bank_reconcile_ext/__manifest__.py
+++ bank_reconcile_ext/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Bank Reconciliation",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Accounting",
     "summary": "Reconcile bank statements with GL account entries",
         "description": "<p>Bank Reconciliation Module allows you to easily reconcile bank statements with GL account entries.</p>",
